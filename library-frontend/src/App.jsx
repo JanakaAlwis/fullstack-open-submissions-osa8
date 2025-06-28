67ffadc89021bbebae48f85a09b39267956042e5
JanakaAlwis <janaka.donmanuwelge@student.hamk.fi>
2025-06-28 20:54:03 +0300

    Exercises 8.17.-8.19 done

--- library-frontend/src/App.jsx
+++ library-frontend/src/App.jsx
@@ -1,27 +1,45 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Authors from "./components/Authors"
 import Books from "./components/Books"
 import NewBook from "./components/NewBook"
 import SetBirthYear from "./components/SetBirthYear"
+import LoginForm from "./components/LoginForm"
 
 const App = () => {
   const [page, setPage] = useState("authors")
+  const [token, setToken] = useState(null)
 
-  let content
-  if (page === "authors") content = <Authors />
-  else if (page === "books") content = <Books />
-  else if (page === "add") content = <NewBook />
-  else if (page === "setBirthYear") content = <SetBirthYear />
+  useEffect(() => {
+    const savedToken = localStorage.getItem('library-user-token')
+    if (savedToken) setToken(savedToken)
+  }, [])
+
+  const logout = () => {
+    setToken(null)
+    localStorage.removeItem('library-user-token')
+    setPage("authors")
+  }
 
   return (
     <div>
       <div>
         <button onClick={() => setPage("authors")}>authors</button>
         <button onClick={() => setPage("books")}>books</button>
-        <button onClick={() => setPage("add")}>add book</button>
-        <button onClick={() => setPage("setBirthYear")}>set birthyear</button>
+        {token && (
+          <>
+            <button onClick={() => setPage("add")}>add book</button>
+            <button onClick={() => setPage("setBirthYear")}>set birthyear</button>
+            <button onClick={logout}>logout</button>
+          </>
+        )}
+        {!token && <button onClick={() => setPage("login")}>login</button>}
       </div>
-      {content}
+
+      {page === "authors" && <Authors />}
+      {page === "books" && <Books />}
+      {page === "add" && token && <NewBook />}
+      {page === "setBirthYear" && token && <SetBirthYear />}
+      {page === "login" && !token && <LoginForm setToken={setToken} />}
     </div>
   )
 }
